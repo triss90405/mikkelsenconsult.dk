12ed425c43c948373f202f32e8c4ce7eaadb147d
Tristan White <oliver.tristan@gmail.com>
2016-02-19 10:36:05 +0100

    Adding responsive navigation

--- mikkelsenconsult.dev/assets/js/functions.js
+++ mikkelsenconsult.dev/assets/js/functions.js
@@ -1,5 +1,6 @@
 $( document ).ready(function() {
 
+
   /* - - - - - Smooth Scrolling  - - - - - */
   var smoothScroll = function() {
     $('a[href*="#"]:not([href="#"])').click(function() {
@@ -8,16 +9,47 @@ $( document ).ready(function() {
         target = target.length ? target : $('[name=' + this.hash.slice(1) +']');
         if (target.length) {
           $('html, body').animate({
-            scrollTop: target.offset().top
+            scrollTop: target.offset().top - 60
           }, 1000);
           return false;
         }
       }
     });
   };
-  
+
+
+  /* - - - - - Sticky navigation when scrolling  - - - - - */
+  var scrolledMenu = function() {
+    $(window).scroll(function() {
+      var nav = $('#sticky-nav');
+      var navInner = $('.nav-items');
+      var yScrollPos = window.pageYOffset;
+      var scrollPosNav = 600;
+      if(yScrollPos > scrollPosNav) {
+        nav.css("top", "0");
+      }
+      else {
+        nav.css("top", "-60px");
+        navInner.removeClass('active');
+      }
+    });
+  };
+
+
+  /* - - - - - Sticky navigation when scrolling  - - - - - */
+  var mobileNav = function() {
+    var toggle = $('.toggle'),
+        nav = $('.nav-items');
+    toggle.on('click', function() {
+      nav.toggleClass('active');
+    })
+  };
+
+
 
   /* - - - - - Call functions - - - - - */
   smoothScroll();
+  scrolledMenu();
+  mobileNav();
 
 });
