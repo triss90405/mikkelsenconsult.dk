666a93af0ad647b6f3727bed8b59f9a7262bed7f
Tristan White <oliver.tristan@gmail.com>
2016-02-20 16:09:31 +0100

    Adding some "where am i" feature to navigation

--- mikkelsenconsult.dev/assets/js/functions.js
+++ mikkelsenconsult.dev/assets/js/functions.js
@@ -46,6 +46,50 @@ $( document ).ready(function() {
   };
 
 
+  /* - - - - - Highlight navigation when scrolling  - - - - - */
+  var highlightNavItem = function() {
+
+    var lastId,
+        topMenu = $('.nav-items'),
+        topMenuHeight = topMenu.outerHeight()+15,
+        // All list items
+        menuItems = topMenu.find("a"),
+        // Anchors corresponding to menu items
+        scrollItems = menuItems.map(function(){
+          var item = $($(this).attr("href"));
+          if (item.length) { return item; }
+        });
+
+    // Bind to scroll
+    $(window).scroll(function(){
+       // Get container scroll position
+       var fromTop = $(this).scrollTop()+topMenuHeight;
+
+       // Get id of current scroll item
+       var cur = scrollItems.map(function(){
+         if ($(this).offset().top < fromTop) {
+           return this;
+         }
+       });
+       // Get the id of the current element
+       cur = cur[cur.length-1];
+       var id = cur && cur.length ? cur[0].id : "";
+
+       if (lastId !== id) {
+           lastId = id;
+
+           // Set/remove active class
+           menuItems
+           .parent().removeClass("active")
+          //  .addClass("active");
+           .end().filter("[href=#"+id+"]").parent().addClass("active");
+
+       }
+    });
+
+  };
+
+
 
   /* - - - - - Form label  - - - - - */
   var formLabel = function() {
@@ -53,12 +97,6 @@ $( document ).ready(function() {
       if( $(this).val().length > 0 ) {
         $(this).next('label').css('top', '-10px');
       }
-      // if( $(this).is(':focus')) {
-      //   $(this).next('label').css('top', '-10px');
-      // }
-      // if( $(this).val().length <= 0 ) {
-      //   $(this).next('label').css('top', '32px');
-      // }
     });
   };
 
@@ -86,6 +124,7 @@ $( document ).ready(function() {
   loadingScreen();
   smoothScroll();
   scrolledMenu();
+  highlightNavItem();
   mobileNav();
   formLabel();
 
