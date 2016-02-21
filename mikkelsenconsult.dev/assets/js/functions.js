b9caf923b3527449453b18ba7fbe94d4203dd0ea
Tristan White <oliver.tristan@gmail.com>
2016-02-21 07:10:11 +0100

    Adding mail script

--- mikkelsenconsult.dev/assets/js/functions.js
+++ mikkelsenconsult.dev/assets/js/functions.js
@@ -103,6 +103,38 @@ $( document ).ready(function() {
 
 
 
+  /* - - - - - Ajax mail - - - - - */
+  var sendMail = function() {
+    var form = $('#ajax-contact');
+    var formMessages = $('#form-messages');
+    $(form).submit(function(event) {
+      event.preventDefault();
+      var formData = $(form).serialize();
+      $.ajax({
+        type: 'POST',
+        url: $(form).attr('action'),
+        data: formData
+      })
+      .done(function(response) {
+        $(formMessages).removeClass('error');
+        $(formMessages).addClass('success');
+        $(formMessages).text(response);
+        $('#name').val('');
+        $('#email').val('');
+        $('#message').val('');
+      })
+      .fail(function(data) {
+        $(formMessages).removeClass('success');
+        $(formMessages).addClass('error');
+        if (data.responseText !== '') {
+          $(formMessages).text(data.responseText);
+        } else {
+          $(formMessages).text('Oops! An error occured and your message could not be sent.');
+        }
+      });
+    });
+  };
+
 
 
   /* - - - - - Call functions - - - - - */
@@ -112,5 +144,7 @@ $( document ).ready(function() {
   highlightNavItem();
   mobileNav();
   formLabel();
+  sendMail();
+
 
 });
---
+++ mikkelsenconsult.dev/mailer.php
@@ -0,0 +1,54 @@
+<?php
+
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+  // Get the form fields and remove whitespace.
+  $name = strip_tags(trim($_POST["name"]));
+	$name = str_replace(array("\r","\n"),array(" "," "),$name);
+  $email = filter_var(trim($_POST["email"]), FILTER_SANITIZE_EMAIL);
+  $message = trim($_POST["message"]);
+
+  // Check that data was sent to the mailer.
+  if ( empty($name) OR empty($message) OR !filter_var($email, FILTER_VALIDATE_EMAIL)) {
+    // Set a 400 (bad request) response code and exit.
+    http_response_code(400);
+    echo "Oops! There was a problem with your submission. Please complete the form and try again.";
+    echo "Oops! lader til at være et problem, udfyld venligs formularen og prøv igen.";
+    exit;
+  }
+
+  // Set the recipient email address.
+  // FIXME: Update this to your desired email address.
+  $recipient = "oliver.tristan@gmail.com";
+
+  // Set the email subject.
+  $subject = "Besked fra $name";
+
+  // Build the email content.
+  $email_content = "<strong>Navn:</strong> $name" . "<br>";
+  $email_content .= "<strong>Email:</strong> $email" . "<br><br>";
+  $email_content .= "<strong>Message:</strong><br>$message" . "<br>";
+
+  // Build the email headers.
+  $email_headers = "From: $name <$email>";
+  $email_headers .= "Reply-To: ". $email . "\r\n";
+  $email_headers .= "MIME-Version: 1.0\r\n";
+  $email_headers .= "Content-Type: text/html; charset=ISO-8859-1\r\n";
+
+  // Send the email.
+  if (mail($recipient, $subject, $email_content, $email_headers)) {
+    // Set a 200 (okay) response code.
+    http_response_code(200);
+    echo "Tak for din henvendelse, jeg vender tilbage hurtigst muligt";
+  } else {
+    // Set a 500 (internal server error) response code.
+    http_response_code(500);
+    echo "Oops! Noget gik galt! Vi har desværre ikke modtaget de besked. Prøv igen.";
+  }
+
+} else {
+  // Not a POST request, set a 403 (forbidden) response code.
+  http_response_code(403);
+  echo "Der lader til at være et problem med formularen, prøv igen.";
+}
+
+?>
